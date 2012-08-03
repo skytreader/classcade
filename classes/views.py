8b24937763f435a228f999044cde4eddbb5b93e9
chad <chadestioco@gmail.com>
2012-08-03 22:49:05 +0800

    Templated views.

--- classes/views.py
+++ classes/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from django.template import Context, loader
+from classes.models import Classes
 
 def index(request):
-	return HttpResponse("ClassCade class index.");
+	classes_list = Classes.objects.all()
+	template = loader.get_template("classes.html")
+	c = Context({"classes" : classes_list})
+	return HttpResponse(template.render(c));
