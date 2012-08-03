14f7bcd5fb7f57ec3203cd32d620859c0b6a3ab6
chad <chadestioco@gmail.com>
2012-08-03 14:47:35 +0800

    Added unicode methods. Used whitespace instead of tabs.

--- classes/models.py
+++ classes/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
 class Classes(models.Model):
-	class_code = models.CharField(max_length=10)
-	section = models.CharField(max_length=5)
+    class_code = models.CharField(max_length=10)
+    section = models.CharField(max_length=5)
+    
+    def __unicode__(self):
+        return self.class_code + " " + self.section
 
 class Students(models.Model):
-	class_code = models.ForeignKey(Classes)
-	student_no = models.IntegerField()
-	first_name = models.CharField(max_length=100)
-	last_name = models.CharField(max_length=100)
+    class_code = models.ForeignKey(Classes)
+    student_no = models.IntegerField()
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    
+    def __unicode__(self):
+        return self.class_code + " " + self.student_no
