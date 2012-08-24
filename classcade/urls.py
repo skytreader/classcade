b0e37449feb4b16a813ca0dab615f650161a4e34
skytreader <chadestioco@gmail.com>
2012-08-25 03:15:20 +0800

    Mapped classcade index URL. Static files remain problematic though.

--- classcade/urls.py
+++ classcade/urls.py
@@ -14,5 +14,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^classcade/classes?', 'classes.views.index')
+    url(r'^classcade/classes?', 'classes.views.index'),
+    url(r'^classcade?', 'index.views.index')
 )
