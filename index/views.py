3bb9e88f83af463dda37cfa218b1c6235815e8cd
skytreader <chadestioco@gmail.com>
2012-08-26 16:33:16 +0800

    Refactored home page using template inheritance.

--- index/views.py
+++ index/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render_to_response
 
 def index(request):
-	return render_to_response("main.html")
+	data = {}
+	data["style_list"] = ["index.css"]
+	return render_to_response("index.html", data)
